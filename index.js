627d56618ec65c11ad9b1b881e31742c22c77041
Brenton <bjackson71@gmail.com>
2020-02-12 18:08:22 -0500

    js experimentation

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 // alert("First JavaScript line!!!")
-document.getElementById("img-caption").innerHTML = "Hello JavaScript!";
+// document.getElementById("img-caption").addEventListener('ondblclick', document.getElementById("img-caption").innerHTML='Hello JavaScript!');
 // document.getElementById("img-caption").innerHTML = "Hello";
+var cap = document.getElementById("img-caption");
+cap.addEventListener('click', capFun);
+function capFun() {
+    this.innerHTML = "Hello JavaScript!";
+}
