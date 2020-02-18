dcc2f047be47a72f4788b4704972ceff3cf9fdd5
Brentonius <bjavonjackson@gmail.com>
2020-02-18 01:42:19 -0500

    removed unnecessary js

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-// alert("First JavaScript line!!!")
-// document.getElementById("img-caption").addEventListener('ondblclick', document.getElementById("img-caption").innerHTML='Hello JavaScript!');
-// document.getElementById("img-caption").innerHTML = "Hello";
-
-var cap = document.getElementById("img-caption");
 var footer = document.getElementById("footer");
 
-cap.addEventListener('click', capFun);
 footer.addEventListener('mouseover',showFoot);
 footer.addEventListener('mouseout', hideFoot);
 footer.addEventListener('click', toProfile);
-
-function capFun() {
-    this.innerHTML = "Hello JavaScript!";
-}
 
 function showFoot() {
     footer.style.opacity = 1;
