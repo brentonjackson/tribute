2492ff90c97c3265d5d871d78cafb079d9d601d6
Brentonius <bjavonjackson@gmail.com>
2020-02-18 01:30:23 -0500

    fixed footer, added footer link to profile, addedjs opacity on mouseover for footer

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 // alert("First JavaScript line!!!")
 // document.getElementById("img-caption").addEventListener('ondblclick', document.getElementById("img-caption").innerHTML='Hello JavaScript!');
 // document.getElementById("img-caption").innerHTML = "Hello";
+
 var cap = document.getElementById("img-caption");
+var footer = document.getElementById("footer");
+
 cap.addEventListener('click', capFun);
+footer.addEventListener('mouseover',showFoot);
+footer.addEventListener('mouseout', hideFoot);
+footer.addEventListener('click', toProfile);
+
 function capFun() {
     this.innerHTML = "Hello JavaScript!";
 }
+
+function showFoot() {
+    footer.style.opacity = 1;
+}
+
+function hideFoot() {
+	this.style.opacity = .75;
+}
+
+function toProfile() {
+	window.location='http://github.com/dabrentmeister/tribute';
+}
+
